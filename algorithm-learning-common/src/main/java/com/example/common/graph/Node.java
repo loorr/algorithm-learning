ddcdee7a07f1ad56badd108f2c6993909d27e725
loorr <zjianfa@qq.com>
2022-03-31 19:10:18 +0800

    add: Graph Node and Double Linked Node

---
+++ algorithm-learning-common/src/main/java/com/example/common/graph/Node.java
@@ -0,0 +1,25 @@
+package com.example.common.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 图节点
+ * @link https://leetcode-cn.com/problems/clone-graph/
+ */
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
