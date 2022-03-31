9e5dab8a2ecdc40dee6754a85d3aca86e81f0aa3
loorr <zjianfa@qq.com>
2022-03-31 16:50:15 +0800

    add: new solution

---
+++ algorithm-learning-loorr/src/main/java/com/example/loorr/onbroding/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,15 @@
+package com.example.loorr.onbroding;
+
+import com.example.common.TemplateCallBack;
+
+public class MaximumDepthOfBinaryTree implements TemplateCallBack {
+    @Override
+    public void generateCase() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
