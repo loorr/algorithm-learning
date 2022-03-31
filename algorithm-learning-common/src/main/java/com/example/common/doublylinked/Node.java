fd236531dc9828c26951dc05afa42e50f8f5aa74
loorr <zjianfa@qq.com>
2022-03-31 19:10:18 +0800

    add: Graph Node and Double Linked Node

---
+++ algorithm-learning-common/src/main/java/com/example/common/doublylinked/Node.java
@@ -0,0 +1,12 @@
+package com.example.common.doublylinked;
+
+/**
+ * 双向链表
+ * @link https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/
+ */
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
