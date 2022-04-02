3c333e2f3e7a700138c8e0511f8eec789f29bfd7
loorr <zjianfa@qq.com>
2022-04-02 22:06:57 +0800

    done

--- algorithm-learning-loorr/src/main/java/com/example/loorr/onbroding/MaximumDepthOfBinaryTree.java
+++ algorithm-learning-loorr/src/main/java/com/example/loorr/onbroding/MaximumDepthOfBinaryTree.java
@@ -1,7 +1,14 @@
 package com.example.loorr.onbroding;
 
 import com.example.common.TemplateCallBack;
+import com.example.common.TreeNode;
+import junit.framework.Assert;
 
+
+/**
+ * 104
+ * @link https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ */
 public class MaximumDepthOfBinaryTree implements TemplateCallBack {
     @Override
     public void generateCase() {
@@ -10,7 +17,14 @@ public class MaximumDepthOfBinaryTree implements TemplateCallBack {
 
     @Override
     public void run() {
-        System.out.println("1212");
+        TreeNode case1 = TreeNode.getInstance(new Integer[]{3,9,20,null,null,15,7});
+        Assert.assertEquals(3, maxDepth(case1));
+    }
 
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
